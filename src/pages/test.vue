65a39174d98e0341b2712c948cdcd2024ffb9121
Sheraz Jutt <sherazarshad419@gmail.com>
2022-12-16 19:49:47 +0500

    updation completed

--- src/pages/test.vue
+++ src/pages/test.vue
@@ -1,39 +1,19 @@
 <template>
   <div class="q-pa-md q-gutter-sm">
-    <q-btn label="With QToolbar" color="primary" @click="toolbar = true" />
-
-
-
-    <q-dialog v-model="toolbar">
-      <q-card>
-        <q-toolbar>
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/logo-v2/svg/logo.svg">
-          </q-avatar>
-
-          <q-toolbar-title><span class="text-weight-bold">Quasar</span> Framework</q-toolbar-title>
-
-          <q-btn flat round dense icon="close" v-close-popup />
-        </q-toolbar>
-
-        <q-card-section>
-          <q-input v-model="min" type="text" label="Minutes" />
-          <q-input v-model="sec" type="text" label="Seconds" />
-          <q-input v-model="milisec" type="text" label="MiliSeconds" />
-        </q-card-section>
-      </q-card>
-    </q-dialog>
+    <q-btn label="Default progress" color="primary" @click="showDefault" />
   </div>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { useQuasar, QSpinnerGears } from 'quasar'
 
 export default {
   setup() {
-    return {
-      toolbar: ref(false)
-    }
+    const $q = useQuasar()
+
+
+
+    return { showDefault }
   }
 }
 </script>
